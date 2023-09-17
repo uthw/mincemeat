068e739c5a7c52bc23a25b5ec21f7f4d0703651c
uthw <73855194+uthw@users.noreply.github.com>
2023-09-16 21:18:59 -0400

    Initial commit

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -1,4 +1,42 @@
 onEvent("item.tooltip", (event) => {
   // tooltip.add()
-  tooltip.add(["industrialforegoing:mob_imprisonment_tool"], "Disabled");
+  // tooltip.add(["industrialforegoing:mob_imprisonment_tool"], "Disabled");
+
+  let akashicTomeBooks = [
+    ""
+  ]
+
+  let summonerBag = [
+    "blue_skies:soulbound_spear",
+    "blue_skies:ethereal_arc",
+    "blue_skies:summoning_table",
+  ]
+
+  let alchemistBag = [
+    "blue_skies:spike_shield",
+    "blue_skies:ventium_ingot",
+    "blue_skies:dusk_arc"
+  ]
+
+  let natureBag = [
+    "blue_skies:nature_arc",
+    "blue_skies:alchemy_table",
+    "blue_skies:falsite_ingot"
+  ]
+
+  let poisonBag = [
+    "blue_skies:poison_arc",
+  ]
+
+  let noLongerNeedsEnergy = [
+    "industrialforegoing:block_breaker",
+    "industrialforegoing:block_placer",
+    "industrialforegoing:fluid_collector"
+  ]
+
+  let powahDisabled = ["powah:furnator_starter", "powah:furnator_basic", "powah:furnator_hardened", "powah:furnator_blazing", "powah:furnator_niotic", "powah:furnator_spirited", "powah:furnator_nitro"]
+
+  tooltip.addAdvanced(noLongerNeedsEnergy, (item, advanced, text) => {
+    text.add(0, Text.of("Does not require power").gold())
+  })
 });
