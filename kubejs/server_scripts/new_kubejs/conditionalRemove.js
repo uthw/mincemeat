341fcc1fcd06bccdba7ed6b3a6cb6e686513bfe7
uthw <73855194+uthw@users.noreply.github.com>
2023-09-24 11:47:47 -0400

    Remove default spell book recipe

--- kubejs/server_scripts/new_kubejs/conditionalRemove.js
+++ kubejs/server_scripts/new_kubejs/conditionalRemove.js
@@ -2,6 +2,10 @@ onEvent("recipes", (event) => {
     let toRemove = [
         { output: "atum:nebu_drop", type: "smelting", type: "blasting" },
         { output: "create:white_sail", input: "#minecraft:wool" },
+        {
+            output: "ars_nouveau:novice_spell_book",
+            input: "minecraft:iron_sword",
+        },
     ];
 
     toRemove.forEach((item) => event.remove(item));
