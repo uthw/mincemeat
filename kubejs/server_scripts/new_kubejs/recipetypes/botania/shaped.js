bebf12b540dc0c52b93f965725bea1e25f3c7bb0
uthw <73855194+uthw@users.noreply.github.com>
2023-09-24 11:48:35 -0400

    Remove mana ring crafting

--- kubejs/server_scripts/new_kubejs/recipetypes/botania/shaped.js
+++ kubejs/server_scripts/new_kubejs/recipetypes/botania/shaped.js
@@ -1,14 +1,14 @@
 onEvent("recipes", (event) => {
     let recipes = [
-        [
-            "botania:mana_ring",
-            ["AB ", "BCB", "  B"],
-            {
-                A: "botania:mana_tablet",
-                B: "#forge:ingots/manasteel",
-                C: "enigmaticlegacy:iron_ring",
-            },
-        ],
+        // [
+        //     "botania:mana_ring",
+        //     ["AB ", "BCB", "  B"],
+        //     {
+        //         A: "botania:mana_tablet",
+        //         B: "#forge:ingots/manasteel",
+        //         C: "enigmaticlegacy:iron_ring",
+        //     },
+        // ],
         [
             "botania:reach_ring",
             ["AB ", "BCB", "  B"],
