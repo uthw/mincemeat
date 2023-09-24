4cc5989f981002d0309c16f71c07c8569d1713d5
uthw <73855194+uthw@users.noreply.github.com>
2023-09-24 11:48:52 -0400

    Add edits for some things

--- kubejs/server_scripts/new_kubejs/recipetypes/create/shaped.js
+++ kubejs/server_scripts/new_kubejs/recipetypes/create/shaped.js
@@ -5,7 +5,39 @@ onEvent("recipes", (event) => {
             ["AB ", "BA ", "   "],
             {
                 A: "minecraft:andesite",
-                B: "environmental:mud_ball",
+                B: "environmental:mud_brick",
+            },
+        ],
+        [
+            "create:millstone",
+            [" A ", "BCB", "DBD"],
+            {
+                A: "#forge:gears/tin",
+                B: "create:andesite_alloy",
+                C: "create:cogwheel",
+                D: "#mysticalagriculture:material/marble",
+            },
+        ],
+        [
+            "create:deployer",
+            ["ABA", "CDC", " E "],
+            {
+                A: "create:electron_tube",
+                B: "immersiveengineering:logic_circuit",
+                C: "create:cogwheel",
+                D: "create:andesite_casing",
+                E: "create:brass_hand",
+            },
+        ],
+        [
+            "3x create:mechanical_crafter",
+            ["ABA", "CDC", "AEA"],
+            {
+                A: "#forge:ingots/bronze",
+                B: "create:electron_tube",
+                C: "#forge:gears/copper",
+                D: "create:copper_casing",
+                E: "#forge:workbench",
             },
         ],
     ];
