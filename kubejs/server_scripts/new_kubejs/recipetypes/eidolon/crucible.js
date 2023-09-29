774e44c13aa1defe391717988542ce4f2b6d94df
uthw <73855194+uthw@users.noreply.github.com>
2023-09-28 20:32:11 -0400

    Comment out since doesn't work

--- kubejs/server_scripts/new_kubejs/recipetypes/eidolon/crucible.js
+++ kubejs/server_scripts/new_kubejs/recipetypes/eidolon/crucible.js
@@ -1,45 +1,45 @@
-onEvent("recipes", (event) => {
-    let recipes = [
-        // result is the item and amount
-        // steps is json with ingredients in chronological order
-        {
-            result: {
-                item: "eidolon:arcane_gold_ingot",
-                count: 2,
-            },
-            steps: [
-                {
-                    ingredients: [
-                        {
-                            tag: "forge:dusts/arcane_crystal",
-                        },
-                        {
-                            tag: "forge:dusts/arcane_crystal",
-                        },
-                        {
-                            item: "eidolon:soul_shard",
-                        },
-                    ],
-                },
-                {
-                    ingredients: [
-                        {
-                            tag: "forge:ingots/gold",
-                        },
-                        {
-                            tag: "forge:ingots/gold",
-                        },
-                    ],
-                },
-            ],
-        },
-    ];
+// onEvent("recipes", (event) => {
+//     let recipes = [
+//         // result is the item and amount
+//         // steps is json with ingredients in chronological order
+//         {
+//             result: {
+//                 item: "eidolon:arcane_gold_ingot",
+//                 count: 2,
+//             },
+//             steps: [
+//                 {
+//                     ingredients: [
+//                         {
+//                             tag: "forge:dusts/arcane_crystal",
+//                         },
+//                         {
+//                             tag: "forge:dusts/arcane_crystal",
+//                         },
+//                         {
+//                             item: "eidolon:soul_shard",
+//                         },
+//                     ],
+//                 },
+//                 {
+//                     ingredients: [
+//                         {
+//                             tag: "forge:ingots/gold",
+//                         },
+//                         {
+//                             tag: "forge:ingots/gold",
+//                         },
+//                     ],
+//                 },
+//             ],
+//         },
+//     ];
 
-    recipes.forEach((recipe) => {
-        event.custom({
-            type: "eidolon:crucible",
-            result: recipe.result,
-            steps: recipe.steps,
-        });
-    });
-});
+//     recipes.forEach((recipe) => {
+//         event.custom({
+//             type: "eidolon:crucible",
+//             result: recipe.result,
+//             steps: recipe.steps,
+//         });
+//     });
+// });
