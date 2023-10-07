b33e148c74bc22bbd0cfebd190747b26484be380
uthw <73855194+uthw@users.noreply.github.com>
2023-10-06 23:37:46 -0400

    Add numina items to this

--- kubejs/server_scripts/new_kubejs/recipetypes/create/mechanical_crafting.js
+++ kubejs/server_scripts/new_kubejs/recipetypes/create/mechanical_crafting.js
@@ -49,6 +49,60 @@ onEvent("recipes", (event) => {
                 E: "#forge:rods/iron",
             },
         },
+        {
+            output: "numina:component_servo",
+            inputs: [" A ", " B ", "CDC", "B "],
+            keys: {
+                A: "numina:component_solenoid",
+                B: "immersiveengineering:component_iron",
+                C: "#forge:plates/steel",
+                D: "create:shaft",
+            },
+        },
+        {
+            output: "numina:component_ion_thruster",
+            inputs: [" AB", "CDE", "FAB"],
+            keys: {
+                A: "numina:component_field_emitter",
+                B: "numina:component_solenoid",
+                C: "immersiveengineering:component_steel",
+                D: "create:shaft",
+                E: "mekanismgenerators:turbine_blade",
+                F: "#forge:wires/copper",
+            },
+        },
+        {
+            output: "numina:component_field_emitter",
+            inputs: [" E ", "ABA", "CDC", " B "],
+            keys: {
+                A: "refinedstorage:quartz_enriched_iron",
+                B: "numina:component_solenoid",
+                C: "thermal:flux_magnet",
+                D: "create:stressometer",
+                E: "immersiveengineering:connector_lv",
+            },
+        },
+        {
+            output: "numina:component_laser_emitter",
+            inputs: ["ABC", "BDB", "EBF"],
+            keys: {
+                A: "#botania:petals/red",
+                B: "create:andesite_alloy",
+                C: "#botania:petals/green",
+                D: "immersivepetroleum:projector",
+                E: "#botania:petals/blue",
+                F: "#botania:petals/yellow",
+            },
+        },
+        {
+            output: "numina:component_control_circuit",
+            inputs: ["AB", "C "],
+            keys: {
+                A: "#forge:wires/copper",
+                B: "immersiveengineering:logic_circuit",
+                C: "#forge:ingots/electrum",
+            },
+        },
     ];
 
     recipes.forEach((recipe) => {
