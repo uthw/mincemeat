88f8e33f1cd71b501a94ca9e50baaafd62600b73
uthw <73855194+uthw@users.noreply.github.com>
2023-10-06 23:36:29 -0400

    Comment out to prevent pigs drop ectoplasm

--- kubejs/server_scripts/loot_tables/forestcraft.js
+++ kubejs/server_scripts/loot_tables/forestcraft.js
@@ -1,15 +1,15 @@
-onEvent("entity.loot_tables", (event) => {
-  event.addEntity("forestcraft:desecrated_soul", (table) => {
-    table.addPool((pool) => {
-      pool.rolls = 1;
-      pool.addItem("iceandfire:ectoplasm", 1).randomChance(0.5);
-    });
-  });
+// onEvent("entity.loot_tables", (event) => {
+//   event.addEntity("forestcraft:desecrated_soul", (table) => {
+//     table.addPool((pool) => {
+//       pool.rolls = 1;
+//       pool.addItem("iceandfire:ectoplasm", 1).randomChance(0.5);
+//     });
+//   });
 
-  event.addEntity("forestcraft:soul_blaze", (table) => {
-    table.addPool((pool) => {
-      pool.rolls = 3;
-      pool.addItem("iceandfire:ectoplasm", 1).randomChance(0.5);
-    });
-  });
-});
+//   event.addEntity("forestcraft:soul_blaze", (table) => {
+//     table.addPool((pool) => {
+//       pool.rolls = 3;
+//       pool.addItem("iceandfire:ectoplasm", 1).randomChance(0.5);
+//     });
+//   });
+// });
