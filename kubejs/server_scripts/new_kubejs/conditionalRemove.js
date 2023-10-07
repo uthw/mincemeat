eda86e825cfc891c33b1bbb97f9891ba073bae2a
uthw <73855194+uthw@users.noreply.github.com>
2023-10-06 23:36:34 -0400

    Don't know

--- kubejs/server_scripts/new_kubejs/conditionalRemove.js
+++ kubejs/server_scripts/new_kubejs/conditionalRemove.js
@@ -2,10 +2,7 @@ onEvent("recipes", (event) => {
     let toRemove = [
         { output: "atum:nebu_drop", type: "smelting", type: "blasting" },
         { output: "create:white_sail", input: "#minecraft:wool" },
-        {
-            output: "ars_nouveau:novice_spell_book",
-            input: "minecraft:iron_sword",
-        },
+        // { id: "ars_nouveau:novice_spell_book"},
         {
             output: "refinedstorage:quartz_enriched_iron",
             input: "minecraft:quartz",
