77a82c6546d5a7f714ed1758b4c16be25463a46f
uthw <73855194+uthw@users.noreply.github.com>
2023-10-13 16:55:22 -0400

    Formatting

--- kubejs/server_scripts/datapack/dimdungeons/chestloot_2.js
+++ kubejs/server_scripts/datapack/dimdungeons/chestloot_2.js
@@ -1,316 +1,286 @@
 // priority: 30
 
-onEvent('server.datapack.high_priority', event => {
+onEvent("server.datapack.high_priority", (event) => {
     let chestLoot = {
-        type: 'minecraft:chest',
+        type: "minecraft:chest",
         pools: [
             {
                 rolls: {
                     min: 2,
-                    max: 2
+                    max: 2,
                 },
-                name: 'dimdungeons:pool_2',
+                name: "dimdungeons:pool_2",
                 entries: [
                     {
-                        type: 'item',
-                        name: 'minecraft:diamond',
+                        type: "item",
+                        name: "minecraft:diamond",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 4,
-                                    max: 9
-                                }
-                            }
-                        ]
+                                    max: 9,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'minecraft:netherite_scrap',
+                        type: "item",
+                        name: "minecraft:netherite_scrap",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 1,
-                                    max: 2
-                                }
-                            }
-                        ]
+                                    max: 2,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'scalinghealth:heart_crystal',
+                        type: "item",
+                        name: "scalinghealth:heart_crystal",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 2,
-                                    max: 3
-                                }
-                            }
-                        ]
+                                    max: 3,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'blue_skies:pyrope_gem',
+                        type: "item",
+                        name: "blue_skies:pyrope_gem",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 4,
-                                    max: 6
-                                }
-                            }
-                        ]
+                                    max: 6,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'blue_skies:aquite_gem',
+                        type: "item",
+                        name: "blue_skies:aquite_gem",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 4,
-                                    max: 6
-                                }
-                            }
-                        ]
+                                    max: 6,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'apotheosis:uncommon_shard',
+                        type: "item",
+                        name: "apotheosis:uncommon_shard",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 1,
-                                    max: 1
-                                }
-                            }
-                        ]
+                                    max: 1,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:silver_sword',
+                        type: "item",
+                        name: "iceandfire:silver_sword",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
+                                    max: 40,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:silver_pickaxe',
+                        type: "item",
+                        name: "iceandfire:silver_pickaxe",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 45,
-                                    max: 45
-                                }
-                            }
-                        ]
+                                    max: 45,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:silver_axe',
+                        type: "item",
+                        name: "iceandfire:silver_axe",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
+                                    max: 40,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:silver_shovel',
+                        type: "item",
+                        name: "iceandfire:silver_shovel",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
+                                    max: 40,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:silver_hoe',
+                        type: "item",
+                        name: "iceandfire:silver_hoe",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 50,
-                                    max: 50
-                                }
-                            }
-                        ]
+                                    max: 50,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:armor_silver_metal_helmet',
+                        type: "item",
+                        name: "iceandfire:armor_silver_metal_helmet",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
+                                    max: 40,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:armor_silver_metal_chestplate',
+                        type: "item",
+                        name: "iceandfire:armor_silver_metal_chestplate",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
+                                    max: 40,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:armor_silver_metal_leggings',
+                        type: "item",
+                        name: "iceandfire:armor_silver_metal_leggings",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
+                                    max: 40,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:armor_silver_metal_boots',
+                        type: "item",
+                        name: "iceandfire:armor_silver_metal_boots",
                         weight: 3,
                         functions: [
                             {
-                                function: 'enchant_with_levels',
+                                function: "enchant_with_levels",
                                 treasure: true,
                                 levels: {
                                     min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
+                                    max: 40,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'spartanweaponry:heavy_crossbow_silver',
-                        weight: 3,
-                        functions: [
-                            {
-                                function: 'enchant_with_levels',
-                                treasure: true,
-                                levels: {
-                                    min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        type: 'item',
-                        name: 'spartanweaponry:longbow_silver',
-                        weight: 3,
-                        functions: [
-                            {
-                                function: 'enchant_with_levels',
-                                treasure: true,
-                                levels: {
-                                    min: 40,
-                                    max: 40
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        type: 'item',
-                        name: 'dimdungeons:item_portal_key',
+                        type: "item",
+                        name: "dimdungeons:item_portal_key",
                         weight: 7,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 1,
-                                    max: 1
-                                }
-                            }
-                        ]
+                                    max: 1,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'dimdungeons:item_secret_bell',
+                        type: "item",
+                        name: "dimdungeons:item_secret_bell",
                         weight: 2,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 1,
-                                    max: 1
-                                }
-                            }
-                        ]
+                                    max: 1,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'dimdungeons:block_portal_crown',
+                        type: "item",
+                        name: "dimdungeons:block_portal_crown",
                         weight: 5,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 1,
-                                    max: 1
-                                }
-                            }
-                        ]
+                                    max: 1,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'loot_table',
-                        name: 'dimdungeons:chests/chestbooks_2',
-                        weight: 10
-                    }
-                ]
-            }
-        ]
+                        type: "loot_table",
+                        name: "dimdungeons:chests/chestbooks_2",
+                        weight: 10,
+                    },
+                ],
+            },
+        ],
     };
 
     event.addJson(`dimdungeons:loot_tables/chests/chestloot_2.json`, chestLoot);
-})
+});
