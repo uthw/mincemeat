dd61fc4ecab091961832a8a41c2e540053795608
uthw <73855194+uthw@users.noreply.github.com>
2023-10-13 16:55:53 -0400

    Initial commit

---
+++ kubejs/server_scripts/new_kubejs/prettypipes/shaped.js
@@ -0,0 +1,12 @@
+// onEvent("recipes", (event) => {
+//     let recipes = [
+//         [
+//             "prettypipes:pipe",
+
+//         ]
+//     ];
+
+//     recipes.forEach((recipe) => {
+//         event.shaped(recipe[0], recipe[1], recipe[2]);
+//     });
+// });
