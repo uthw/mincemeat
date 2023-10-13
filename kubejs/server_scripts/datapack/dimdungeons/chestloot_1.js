ca05bfc9195cc5426f3595a27bb213a9bd345cd2
uthw <73855194+uthw@users.noreply.github.com>
2023-10-13 16:55:15 -0400

    Adjustments

--- kubejs/server_scripts/datapack/dimdungeons/chestloot_1.js
+++ kubejs/server_scripts/datapack/dimdungeons/chestloot_1.js
@@ -1,165 +1,221 @@
 // priority: 30
 
-onEvent('server.datapack.high_priority', event => {
+onEvent("server.datapack.high_priority", (event) => {
     let chestLoot = {
-        type: 'minecraft:chest',
+        type: "minecraft:chest",
         pools: [
             {
                 rolls: {
                     min: 2,
-                    max: 2
+                    max: 2,
                 },
-                name: 'dimdungeons:pool_1',
+                name: "dimdungeons:pool_1",
                 entries: [
                     {
-                        type: 'item',
-                        name: 'minecraft:iron_ingot',
+                        type: "item",
+                        name: "minecraft:iron_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 10,
-                                    max: 16
-                                }
-                            }
-                        ]
+                                    max: 16,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'minecraft:gold_ingot',
+                        type: "item",
+                        name: "minecraft:gold_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 10,
-                                    max: 16
-                                }
-                            }
-                        ]
+                                    max: 16,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:silver_ingot',
+                        type: "item",
+                        name: "iceandfire:silver_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 10,
-                                    max: 16
-                                }
-                            }
-                        ]
+                                    max: 16,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'eidolon:lead_ingot',
+                        type: "item",
+                        name: "eidolon:lead_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 10,
-                                    max: 16
-                                }
-                            }
-                        ]
+                                    max: 16,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'blue_skies:falsite_ingot',
+                        type: "item",
+                        name: "blue_skies:falsite_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 4,
-                                    max: 8
-                                }
-                            }
-                        ]
+                                    max: 8,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'blue_skies:ventium_ingot',
+                        type: "item",
+                        name: "blue_skies:ventium_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 4,
-                                    max: 8
-                                }
-                            }
-                        ]
+                                    max: 8,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'blue_skies:horizonite_ingot',
+                        type: "item",
+                        name: "blue_skies:horizonite_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 10,
-                                    max: 16
-                                }
-                            }
-                        ]
+                                    max: 16,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'eidolon:pewter_ingot',
+                        type: "item",
+                        name: "eidolon:pewter_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 8,
-                                    max: 14
-                                }
-                            }
-                        ]
+                                    max: 14,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'iceandfire:copper_ingot',
+                        type: "item",
+                        name: "iceandfire:copper_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 12,
-                                    max: 18
-                                }
-                            }
-                        ]
+                                    max: 18,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'item',
-                        name: 'eidolon:arcane_gold_ingot',
+                        type: "item",
+                        name: "eidolon:arcane_gold_ingot",
                         weight: 3,
                         functions: [
                             {
-                                function: 'set_count',
+                                function: "set_count",
                                 count: {
                                     min: 6,
-                                    max: 10
-                                }
-                            }
-                        ]
+                                    max: 10,
+                                },
+                            },
+                        ],
                     },
                     {
-                        type: 'loot_table',
-                        name: 'dimdungeons:chests/chestbooks_1',
-                        weight: 20
-                    }
-                ]
-            }
-        ]
+                        type: "item",
+                        name: "thermal:tin_ingot",
+                        weight: 3,
+                        functions: [
+                            {
+                                function: "set_count",
+                                count: {
+                                    min: 8,
+                                    max: 16,
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        type: "item",
+                        name: "immersiveengineering:aluminum_ingot",
+                        weight: 3,
+                        functions: [
+                            {
+                                function: "set_count",
+                                count: {
+                                    min: 8,
+                                    max: 16,
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        type: "item",
+                        name: "undergarden:cloggrum_ingot",
+                        weight: 3,
+                        functions: [
+                            {
+                                function: "set_count",
+                                count: {
+                                    min: 6,
+                                    max: 10,
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        type: "item",
+                        name: "undergarden:froststeel_ingot",
+                        weight: 3,
+                        functions: [
+                            {
+                                function: "set_count",
+                                count: {
+                                    min: 6,
+                                    max: 10,
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        type: "loot_table",
+                        name: "dimdungeons:chests/chestbooks_1",
+                        weight: 20,
+                    },
+                ],
+            },
+        ],
     };
 
     event.addJson(`dimdungeons:loot_tables/chests/chestloot_1.json`, chestLoot);
-})
+});
