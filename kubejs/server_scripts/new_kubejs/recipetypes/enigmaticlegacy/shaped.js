b794197a59c03f9ee6b0981711da9ec916e58903
uthw <73855194+uthw@users.noreply.github.com>
2023-10-13 16:56:45 -0400

    New recipes for enigmaticlegacy

--- kubejs/server_scripts/new_kubejs/recipetypes/enigmaticlegacy/shaped.js
+++ kubejs/server_scripts/new_kubejs/recipetypes/enigmaticlegacy/shaped.js
@@ -45,6 +45,43 @@ onEvent("recipes", (event) => {
                 F: "iceandfire:fire_dragon_heart",
             },
         ],
+        [
+            "enigmaticlegacy:magnet_ring",
+            [" A ", "BCD", " E "],
+            {
+                A: "#forge:ingots/nickel",
+                B: "#forge:ingots/tin",
+                C: "enigmaticlegacy:iron_ring",
+                D: "#forge:ingots/zinc",
+                E: "#forge:ingots/silver",
+            },
+        ],
+        [
+            "enigmaticlegacy:super_magnet_ring",
+            [" A ", "BCD", " E "],
+            {
+                A: "minecraft:ender_eye",
+                B: "betterendforge:thallasium_ingot",
+                C: "enigmaticlegacy:magnet_ring",
+                D: "betterendforge:terminite_ingot",
+                E: "endergetic:booflo_hide",
+            },
+        ],
+        [
+            "enigmaticlegacy:infinimeal",
+            ["ABC", "DEF", "GHI"],
+            {
+                A: "neapolitan:adzuki_beans",
+                B: "blue_skies:winter_leaf_seeds",
+                C: "blue_skies:fiery_bean_seeds",
+                D: "atum:emmer_seeds",
+                E: "enigmaticlegacy:earth_heart",
+                F: "undergarden:gloomgourd_seeds",
+                G: "infernalexp:glownuggets",
+                H: "ars_nouveau:mana_bloom_crop",
+                I: "minecraft:nether_wart",
+            },
+        ],
     ];
 
     recipes.forEach((recipe) => {
