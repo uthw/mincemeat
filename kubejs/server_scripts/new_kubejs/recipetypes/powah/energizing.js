d3b681e8abd650c2c9f040551cb4fcf7478720fd
uthw <73855194+uthw@users.noreply.github.com>
2023-10-14 22:33:59 -0400

    Add recipe for Energy Battery

--- kubejs/server_scripts/new_kubejs/recipetypes/powah/energizing.js
+++ kubejs/server_scripts/new_kubejs/recipetypes/powah/energizing.js
@@ -7,13 +7,24 @@ onEvent("recipes", (event) => {
         },
         {
             ingredients: [
-                { item: "#tconstruct:tables" },
+                { tag: "tconstruct:tables" },
                 { tag: "forge:ingots/electrum" },
                 { tag: "forge:ingots/electrum" },
                 { item: "modularrouters:blank_module" },
             ],
             energy: 150000,
             result: "powersuits:tinkertable",
+        },
+        {
+            ingredients: [
+                { item: "integrateddynamics:crystalized_menril_chunk" },
+                { item: "integrateddynamics:crystalized_menril_block" },
+                { item: "integrateddynamics:capacitor_mv" },
+                { item: "integrateddynamics:crystalized_menril_block" },
+                { item: "integrateddynamics:crystalized_menril_chunk" },
+            ],
+            energy: 100000,
+            result: "integrateddynamics:energy_battery",
         },
     ];
 
