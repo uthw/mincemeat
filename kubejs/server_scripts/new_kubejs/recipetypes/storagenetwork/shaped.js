37b44e7841c24729cd4defdc421c2f4437081fc7
uthw <73855194+uthw@users.noreply.github.com>
2023-10-14 22:34:07 -0400

    Adjust recipes

--- kubejs/server_scripts/new_kubejs/recipetypes/storagenetwork/shaped.js
+++ kubejs/server_scripts/new_kubejs/recipetypes/storagenetwork/shaped.js
@@ -5,8 +5,8 @@ onEvent("recipes", (event) => {
             ["ABA", "BCB", "ABA"],
             {
                 A: "#storagedrawers:drawers",
-                B: "environmental:ice_chain",
-                C: "#forge:gems/arcane_crystal",
+                B: "#supplementaries:deepslate",
+                C: "minecraft:diamond",
             },
         ],
         [
@@ -14,10 +14,32 @@ onEvent("recipes", (event) => {
             ["ABA", "BCB", "ABA"],
             {
                 A: "#forge:nuggets/brass",
-                B: "environmental:ice_chain",
-                C: "blue_skies:diopside_gem",
+                B: "#supplementaries:deepslate",
+                C: "minecraft:diamond",
             },
         ],
+        [
+            "storagenetwork:inventory_remote",
+            ["A  ", "BCB", "DED"],
+            {
+                A: "immersiveengineering:connector_lv",
+                B: "create:andesite_alloy",
+                C: "#forge:gears/copper",
+                D: "#forge:ingots/aluminum",
+                E: "storagenetwork:master"
+            }
+        ],
+        [
+            "storagenetwork:crafting_remote",
+            ["A  ", "BCB", "DED"],
+            {
+                A: "immersiveengineering:connector_mv",
+                B: "#forge:silicon",
+                C: "#charm:crafting_tables",
+                D: "#forge:ingots/aluminum",
+                E: "storagenetwork:master"
+            }
+        ]
     ];
 
     recipes.forEach((recipe) => {
